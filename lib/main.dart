a663d164cc8a9c055cb402a4597247a8c455a729
Sinan <sinanac124@gmail.com>
2022-07-17 19:05:27 +0530

    Camera app completed

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:camera_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    createList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+        ),
+        home: const HomeScreen());
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,100 @@
+import 'dart:io';
+import 'package:camera_app/screens/image_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+
+ValueNotifier<List> imageList = ValueNotifier([]);
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+final imagePick = ImagePicker();
+
+pickImage() async {
+  final imagePick = await ImagePicker().pickImage(source: ImageSource.camera);
+  if (imagePick == null) {
+    return;
+  } else {
+    var dir = await getExternalStorageDirectory();
+    File imagePath = File(imagePick.path);
+    final newImage = await imagePath.copy('${dir!.path}/${DateTime.now()}.jpg');
+    imageList.value.add(newImage.path);
+    imageList.notifyListeners();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Camera App'),
+        centerTitle: true,
+      ),
+      floatingActionButton: Container(
+        height: 100,
+        width: 100,
+        padding: const EdgeInsets.only(bottom: 30),
+        child: FloatingActionButton(
+          onPressed: () {
+            pickImage();
+          },
+          child: const Icon(
+            Icons.camera_enhance_outlined,
+            size: 35,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: ValueListenableBuilder(
+        valueListenable: imageList,
+        builder: ((BuildContext context, List data, Widget? _) {
+          return GridView.builder(
+            itemCount: data.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, mainAxisSpacing: 3, crossAxisSpacing: 3),
+            itemBuilder: (BuildContext context, index) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ImageScreen(img: data[index]),
+                    ),
+                  );
+                },
+                child: Hero(
+                  tag: index,
+                  child: Image(
+                    fit: BoxFit.fill,
+                    image: FileImage(
+                      File(data[index]),
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        }),
+      ),
+    );
+  }
+}
+
+createList() async {
+  final dir = await getExternalStorageDirectory();
+  if (dir != null) {
+    var listDir = await dir.list().toList();
+    for (var i = 0; i < listDir.length; i++) {
+      if (listDir[i].path.endsWith('.jpg')) {
+        imageList.value.add(listDir[i].path);
+        imageList.notifyListeners();
+      }
+    }
+  }
+}
