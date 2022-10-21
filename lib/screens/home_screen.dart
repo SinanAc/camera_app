c0b96baac9189dc94d28bab11079a5b341ab93dc
Sinan <sinanac124@gmail.com>
2022-10-21 23:36:33 +0530

    problem solved

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_use_of_protected_member, invalid_use_of_visible_for_testing_member
+
 import 'dart:io';
 import 'package:camera_app/screens/image_screen.dart';
 import 'package:flutter/material.dart';
